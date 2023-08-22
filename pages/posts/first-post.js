420c1bfd7400a95382d9d2e5c0068a6ee880fe41
Carlos Javier <76453806+carlosjpp99@users.noreply.github.com>
2023-08-22 11:01:01 -0400

    Finished Navigate Between Pages section from Next.js Foundation Course - Worked on Pages in Next.js - Worked on Link Component - Worked on Client-Side Navigation - Worked on Next.js Code Splitting and prefetching

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+export default function firstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+    </>
+  )
+}
