bd4c3035de4c5c3ba95aa8af269f64403c89a0ac
Carlos Javier <76453806+carlosjpp99@users.noreply.github.com>
2023-08-23 10:49:24 -0400

    Finished Assets, Metadata, and CSS section - At pages/index.js => Remove boilerplate code and transform almost entirely - At pages/posts/first-post.js => Create a fake post with profile picture and a lorem ipsum text in it. - At styles/globals.css => Only some css class for following along the tutorial

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,24 +1,26 @@
 import Head from "next/head"
 import Link from "next/link"
-import Script from "next/script"
+// import Script from "next/script"
+import Layout from "../components/layout"
 export default function firstPost() {
   return (
     <>
-      <Head>
-        <title>My first post</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to Home</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>My first post</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        {/* Script component is only for make an example */}
+        {/* <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        /> */}
+        <h1>First Post</h1>
+        <p>Sed aliquam voluptua eirmod. Quis ipsum ut et sed tempor amet ad sed lorem nonumy stet nulla feugait dolores euismod dolore. Stet esse justo magna consetetur blandit ipsum esse gubergren illum enim kasd. Vel sadipscing labore dolor elitr consetetur et nulla sed congue et. Te duo dolor clita et dolor nonumy no et stet volutpat. Nonumy exerci sit clita dignissim nulla dolore amet nonummy consetetur nostrud lorem tincidunt in voluptua vel erat sanctus.</p>
+      </Layout>
     </>
   )
 }
